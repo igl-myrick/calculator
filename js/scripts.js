6689f79357ab1f77a438da63b5b13c237cc818c6
igl-myrick <igl.myrick@gmail.com>
2023-10-31 13:24:21 -0700

    rewrite page to use radio form for calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,42 @@
 // business logic
-function add(number1, number2) {
-  return number1 + number2;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(number1, number2) {
-  return number1 * number2;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(number1, number2) {
-  return number1 / number2;
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
 // ui logic
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
 
-window.alert(number1 + " + " + number2 + " = " + add(number1, number2) + ", " + number1 + " - " + number2 + " = " + subtract(number1, number2) + ", " + number1 + " x " + number2 + " = " + multiply(number1, number2) + ", " + number1 + " % " + number2 + " = " + divide(number1, number2) + ".");
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
+
+  document.getElementById("output").innerText = result;
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator")
+  form.addEventListener("submit", handleCalculation);
+});
